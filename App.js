4f3eb43f1d722c08218f8c9bd8fd53c40e1c550c
bitbucket ofc <pankajkumar199528@gmail.com>
2023-10-19 15:14:17 +0530

    my react app

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
 const heading = React.createElement("h1",{id:"heading", abc:"abc"}, "Hello World! from React")
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(heading);
-console.log(heading)
